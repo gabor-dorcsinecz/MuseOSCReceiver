e6bf37604e0452d1fc6fe3cf21d0441c136e2855
Dorcsinecz Gabor <gabor.dorcsinecz@digital.hmrc.gov.uk>
2020-07-14 19:37:48 +0200

    Added TimeTag

--- src/main/scala/com/zorgonout/museoscreceiver/OSCParser.scala
+++ src/main/scala/com/zorgonout/museoscreceiver/OSCParser.scala
@@ -18,13 +18,23 @@ object OSC {
   //case class OSCBundle(timeTag: String, elements: String*) extends OSCPackage
 
   case class OSCMessage(data:ByteVector) extends OSCPackage
-  case class OSCBundle(data:ByteVector) extends OSCPackage
+  case class OSCBundle(data:String, timeTag:Long) extends OSCPackage
 
   val packageTypeCodec = enumerated(uint8, OSCPackegeType)
+
+  val timeTagCodec = ulong(8)
+
+  //constant(ByteVector("bundle".getBytes("ascii")))  //Error:(28, 7) Could not prove that Unit can be converted to/from com.zorgonout.museoscreceiver.OSC.OSCBundle).as[OSCBundle]
+  val bundleCodec:Codec[OSCBundle] = (
+    fixedSizeBytes(7,utf8) ::
+    timeTagCodec
+  ).as[OSCBundle]
+
+
   val packageCodec = discriminated[OSCPackage]
     .by(packageTypeCodec)
     //.caseO(OSCPackegeType.MESSAGE)(pkg => pkg.)
     .|(OSCPackegeType.MESSAGE){case m:OSCMessage => m.data}(a => OSCMessage(a))(bytes)
-    .|(OSCPackegeType.BUNDLE){case m:OSCBundle => m.data}(a => OSCBundle(a))(bytes)
+    .|(OSCPackegeType.BUNDLE){case m:OSCBundle => m}(identity)(bundleCodec)
 
 }
--- src/test/scala/com/zorgonout/museoscreceiver/OSCParserSpec.scala
+++ src/test/scala/com/zorgonout/museoscreceiver/OSCParserSpec.scala
@@ -8,12 +8,13 @@ class OSCParserSpec extends AnyWordSpec with should.Matchers {
   "it" should {
     "Parse one message" in {
       val oneChunk = MuseIncomingOSC.example1(0)
-      //println(oneChunk.map(a => a.toHexString))
-      //println(oneChunk.map(a => a.toChar))
+      println(oneChunk.map(a => a.toHexString))
+      println(oneChunk.map(a => a.toChar))
       //println(MuseIncomingOSC.example1.map(a => a.map(b => b.toChar)).mkString("\r\n"))
       val decoded = OSC.packageCodec.decode(oneChunk.toBitVector)
-      //println(decoded)
+      println(decoded)
       decoded.isSuccessful shouldBe true
+
     }
   }
 }
