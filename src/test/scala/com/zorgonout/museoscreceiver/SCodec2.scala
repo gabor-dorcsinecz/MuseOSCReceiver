0d4223748b65755c498ac07cc8606fb06dfe94a6
Dorcsinecz Gabor <gabor.dorcsinecz@digital.hmrc.gov.uk>
2020-07-14 15:30:00 +0200

    Added scodec discriminated test case

--- src/test/scala/com/zorgonout/museoscreceiver/SCodec2.scala
+++ src/test/scala/com/zorgonout/museoscreceiver/SCodec2.scala
@@ -2,6 +2,7 @@ package com.zorgonout.museoscreceiver
 
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
+import scodec.{Codec, DecodeResult}
 import scodec.codecs._
 
 class SCodec2 extends AnyWordSpec with should.Matchers {
@@ -16,6 +17,33 @@ class SCodec2 extends AnyWordSpec with should.Matchers {
       val encodedAccount = accountCodec.encode(account)
       val decodecAccount = accountCodec.decode(encodedAccount.require)
       decodecAccount.require.value shouldBe account
+    }
+  }
+
+  "discriminated" should {
+    "discriminate based on Enumeration" in {
+      object VersionType extends Enumeration {
+        val MSG = Value(47,"/")
+        val BUNDLE = Value(35,"#")
+      }
+
+      sealed trait OCSPackt
+      case class Message(name:String) extends OCSPackt
+      case class Bundle(name:String) extends OCSPackt
+
+      val versionTypeCodec = enumerated(uint8, VersionType)
+
+      val versionCodec = discriminated[OCSPackt]
+        .by(versionTypeCodec)
+        .|(VersionType.MSG) {case m:Message => m.name} (a => Message(a))(utf8)
+        .|(VersionType.BUNDLE) {case b:Bundle => b.name} (a => Bundle(a))(utf8)
+
+      val message = Message("Message")
+      val encoded = versionCodec.encode(message)   //Successful(BitVector(64 bits, 0x2f4d657373616765))
+      val printed = encoded.require.bytes.toSeq.map(a => a.toChar).mkString("")  //   "/Message"
+      printed shouldBe "/Message"
+      val decoded = versionCodec.decode(encoded.require)  //Successful(DecodeResult(Message(Message),BitVector(empty)))
+      decoded.require.value shouldBe  message
     }
   }
 }
